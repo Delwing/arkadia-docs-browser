5851c4dd0e0fb3c038468808f683adfe508f0c18
Piotr Wilczynski <delwing@gmail.com>
2024-10-07 23:30:49 +0200

    fix path

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import parseDocs from "./src/build/parse-docs"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '',
   plugins: [react(), parseDocs()],
   resolve: {
     alias: {
